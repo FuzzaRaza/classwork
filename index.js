daf1febbfdbd0dfe7df653ce16593920a27e514d
FuzzaRaza <48881328+FuzzaRaza@users.noreply.github.com>
2019-03-24 20:20:45 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,18 @@
  */
 
 import { AppRegistry } from 'react-native';
-import Basic from './src/components/Basic';
+import HelloWorld from './src/components/Helloworld';
+import Props from './src/components/Props';
+import State from './src/components/State';
+import Fetching from './src/components/Fetching';
+import SectionWiseView from './src/components/SectionWiseView';
 import { name as appName } from './app.json';
+import ScrollView from './src/components/ScrollView';
+import HandlingTouch from './src/components/HandlingTouch';
+import TextHandling from './src/components/TextHandling';
+import FlexboxLayout from './src/components/FLexboxLayout';
+import HeightWidth from './src/components/HeightWidth';
+import Styling from './src/components/Styling';
 
-AppRegistry.registerComponent(appName, () => Basic);
+
+AppRegistry.registerComponent(appName, () => Styling);
